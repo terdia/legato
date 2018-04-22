0cef2d6131f79e7c9ffb4c8e056c712c5dcc0ccd
Osayawe Ogbemudia Terry <terrymarcy2000@yahoo.com>
2018-04-22 23:36:17 +0800

    Add unit test

--- cache/blade/4aa9b31e5d2e6e93672b16f1e6d6b8e9cfaef9ae.php
+++ cache/blade/4aa9b31e5d2e6e93672b16f1e6d6b8e9cfaef9ae.php
@@ -3,7 +3,7 @@
 <?php $__env->startSection('body'); ?>
     <div class="text-center">
         <h1><?php echo e(getenv('APP_NAME')); ?> v1</h1>
-        <p>Start building that next app.</p>
+        <p>Start building that next appx</p>
     </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.base', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
