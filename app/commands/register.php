62bf5a4baf5310b2033c9a39251ef77f12841ccd
Osayawe Ogbemudia Terry <terrymarcy2000@yahoo.com>
2018-04-28 04:29:49 +0800

    Add ability to create a run console commands
    Add ability to create a run console commands, Remove blade cache files

---
+++ app/commands/register.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    \App\Command\Welcome::class,
+];
---
+++ tests/ConsoleTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ConsoleTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function create_something()
+    {
+        $this->assertTrue(true);
+    }
+
+}
