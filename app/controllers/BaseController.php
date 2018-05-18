9101a876f9e1484597c84747ff91426d5de22852
Osayawe Ogbemudia Terry <terrymarcy2000@yahoo.com>
2018-05-19 02:17:25 +0800

    Add CSRF Verification support

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,9 +1,7 @@
 <?php
 namespace App\Controllers;
 use Legato\Framework\Controllers\BaseController as Controller;
-use Legato\Framework\Session\SessionManager;
 
 class BaseController extends Controller{
 
-    use SessionManager;
 }
---
+++ app/middleware/CSRFVerification.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Middleware;
+
+use Legato\Framework\Security\CSRFProtection;
+
+class CSRFVerification extends CSRFProtection
+{
+
+    /**
+     * URIs that you want to remove from CSRF Verification
+     * e.g /thanks which should match your route target path
+     *
+     * @var array
+     */
+    protected $skipValidation = [
+
+    ];
+}
